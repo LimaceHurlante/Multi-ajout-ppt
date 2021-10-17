db5e3050fc7a8176c56d8feadabc96c5984b99cf
Zacharie Cortes <zacharie.cortes@me.com>
2021-10-17 11:34:13 +0200

    Ajoutez des fichiers projet.

---
+++ Form1.Designer.vb
@@ -0,0 +1,142 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form remplace la méthode Dispose pour nettoyer la liste des composants.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requise par le Concepteur Windows Form
+    Private components As System.ComponentModel.IContainer
+
+    'REMARQUE : la procédure suivante est requise par le Concepteur Windows Form
+    'Elle peut être modifiée à l'aide du Concepteur Windows Form.  
+    'Ne la modifiez pas à l'aide de l'éditeur de code.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.ButtonPickFile = New System.Windows.Forms.Button()
+        Me.ButtonPickFolder = New System.Windows.Forms.Button()
+        Me.IncludeSubFolder = New System.Windows.Forms.CheckBox()
+        Me.EndSlide = New System.Windows.Forms.RadioButton()
+        Me.StartSlide = New System.Windows.Forms.RadioButton()
+        Me.ButtonRun = New System.Windows.Forms.Button()
+        Me.ButtonExit = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'ButtonPickFile
+        '
+        Me.ButtonPickFile.Location = New System.Drawing.Point(12, 12)
+        Me.ButtonPickFile.Name = "ButtonPickFile"
+        Me.ButtonPickFile.Size = New System.Drawing.Size(292, 31)
+        Me.ButtonPickFile.TabIndex = 0
+        Me.ButtonPickFile.Text = "Choisir le fichier contenant la slide à rajouter"
+        Me.ButtonPickFile.UseVisualStyleBackColor = True
+        '
+        'ButtonPickFolder
+        '
+        Me.ButtonPickFolder.Location = New System.Drawing.Point(12, 49)
+        Me.ButtonPickFolder.Name = "ButtonPickFolder"
+        Me.ButtonPickFolder.Size = New System.Drawing.Size(292, 31)
+        Me.ButtonPickFolder.TabIndex = 1
+        Me.ButtonPickFolder.Text = "Choisir le dossier contenant les ppt à modifier"
+        Me.ButtonPickFolder.UseVisualStyleBackColor = True
+        '
+        'IncludeSubFolder
+        '
+        Me.IncludeSubFolder.AutoSize = True
+        Me.IncludeSubFolder.Checked = True
+        Me.IncludeSubFolder.CheckState = System.Windows.Forms.CheckState.Checked
+        Me.IncludeSubFolder.Location = New System.Drawing.Point(337, 63)
+        Me.IncludeSubFolder.Name = "IncludeSubFolder"
+        Me.IncludeSubFolder.Size = New System.Drawing.Size(140, 17)
+        Me.IncludeSubFolder.TabIndex = 4
+        Me.IncludeSubFolder.Text = "Inclure les sous-dossiers"
+        Me.IncludeSubFolder.UseVisualStyleBackColor = True
+        '
+        'EndSlide
+        '
+        Me.EndSlide.AutoSize = True
+        Me.EndSlide.Checked = True
+        Me.EndSlide.Location = New System.Drawing.Point(327, 40)
+        Me.EndSlide.Name = "EndSlide"
+        Me.EndSlide.Size = New System.Drawing.Size(165, 17)
+        Me.EndSlide.TabIndex = 3
+        Me.EndSlide.TabStop = True
+        Me.EndSlide.Text = "Ajouter la slide à la fin des ppt"
+        Me.EndSlide.UseVisualStyleBackColor = True
+        '
+        'StartSlide
+        '
+        Me.StartSlide.AutoSize = True
+        Me.StartSlide.Location = New System.Drawing.Point(322, 19)
+        Me.StartSlide.Name = "StartSlide"
+        Me.StartSlide.Size = New System.Drawing.Size(176, 17)
+        Me.StartSlide.TabIndex = 2
+        Me.StartSlide.Text = "Ajouter la slide au début des ppt"
+        Me.StartSlide.UseVisualStyleBackColor = True
+        '
+        'ButtonRun
+        '
+        Me.ButtonRun.Location = New System.Drawing.Point(137, 102)
+        Me.ButtonRun.Name = "ButtonRun"
+        Me.ButtonRun.Size = New System.Drawing.Size(355, 31)
+        Me.ButtonRun.TabIndex = 5
+        Me.ButtonRun.Text = "Lancer le rajout multiple"
+        Me.ButtonRun.UseVisualStyleBackColor = True
+        '
+        'ButtonExit
+        '
+        Me.ButtonExit.Location = New System.Drawing.Point(12, 102)
+        Me.ButtonExit.Name = "ButtonExit"
+        Me.ButtonExit.Size = New System.Drawing.Size(107, 31)
+        Me.ButtonExit.TabIndex = 6
+        Me.ButtonExit.Text = "Quitter"
+        Me.ButtonExit.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(494, 139)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(13, 13)
+        Me.Label1.TabIndex = 7
+        Me.Label1.Text = "?"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(513, 155)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.ButtonExit)
+        Me.Controls.Add(Me.ButtonRun)
+        Me.Controls.Add(Me.StartSlide)
+        Me.Controls.Add(Me.EndSlide)
+        Me.Controls.Add(Me.IncludeSubFolder)
+        Me.Controls.Add(Me.ButtonPickFolder)
+        Me.Controls.Add(Me.ButtonPickFile)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents ButtonPickFile As Button
+    Friend WithEvents ButtonPickFolder As Button
+    Friend WithEvents IncludeSubFolder As CheckBox
+    Friend WithEvents EndSlide As RadioButton
+    Friend WithEvents StartSlide As RadioButton
+    Friend WithEvents ButtonRun As Button
+    Friend WithEvents ButtonExit As Button
+    Friend WithEvents Label1 As Label
+End Class
---
+++ Form1.vb
@@ -0,0 +1,306 @@
+﻿
+Imports Office = Microsoft.Office.Core
+Imports Graph = Microsoft.Office.Interop.Graph
+Imports PowerPoint = Microsoft.Office.Interop.PowerPoint
+Public Class Form1
+    Public FicherPPTdeBASE As String = "", DossierExport As String = "", OnYVa As Boolean, dlgOpen As FileDialog, dlgSave As FileDialog
+    Public Shared PwP As PowerPoint.Application
+    Public PresDeBase As PowerPoint.Presentation, SlideDeBase As PowerPoint.Slide
+    Public PresAModifer As PowerPoint.Presentation, SlideAModifer As PowerPoint.Slide
+
+
+
+    Private Sub CheckIfReadyToGo()
+        ButtonRun.Enabled = (Not String.IsNullOrEmpty(FicherPPTdeBASE)) And (Not String.IsNullOrEmpty(DossierExport))
+        If ButtonRun.Enabled Then ButtonRun.Select()
+    End Sub
+    Private Sub ButtonRun_Click(sender As Object, e As EventArgs) Handles ButtonRun.Click
+
+        Call main()
+
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+        Call CheckIfReadyToGo()
+    End Sub
+
+    Private Sub ButtonPickFile_Click(sender As Object, e As EventArgs) Handles ButtonPickFile.Click
+        FicherPPTdeBASE = FileAddress()
+        If FicherPPTdeBASE IsNot "" Then
+            MsgBox("Vous avez selectionné ce fichier :" & vbCrLf & FicherPPTdeBASE)
+        Else
+            MsgBox("Pas de fichier séléctioné ...")
+        End If
+        Call CheckIfReadyToGo()
+    End Sub
+    Private Function FileAddress() As String
+        Dim ofd As New OpenFileDialog With {
+            .Multiselect = False,
+            .Title = "Choix du fichier PowerPoint à ajouter",
+            .Filter = "Fichiers PowerPoint|*.ppt;*.pptx;*.pptm"
+        }
+
+        If ofd.ShowDialog = Windows.Forms.DialogResult.OK Then
+            Return ofd.FileName
+        Else
+            Return String.Empty
+        End If
+    End Function
+
+
+
+    Private Sub ButtonPickFolder_Click(sender As Object, e As EventArgs) Handles ButtonPickFolder.Click
+        DossierExport = FolderAddress()
+        If DossierExport IsNot "" Then
+            MsgBox("Vous avez selectionné ce dossier :" & vbCrLf & DossierExport)
+        Else
+            MsgBox("Pas de dossier séléctioné ...")
+        End If
+        Call CheckIfReadyToGo()
+    End Sub
+    Private Function FolderAddress() As String
+        Dim dlg As New FolderBrowserDialog With {
+            .Description = "Selectionnez le dossier dans lequel se trouvent les ppt à modifer"
+        }
+        If dlg.ShowDialog() = DialogResult.OK Then
+            Dim path As String = dlg.SelectedPath
+            Return path
+        Else
+            Return String.Empty
+        End If
+    End Function
+
+    Private Sub ButtonExit_Click(sender As Object, e As EventArgs) Handles ButtonExit.Click
+        Application.Exit()
+    End Sub
+    Sub Main()
+        'Start Powerpoint
+        PwP = New PowerPoint.Application()
+
+
+
+        PresDeBase = PwP.Presentations.Open(FicherPPTdeBASE, , , False)
+        PresDeBase.Slides(1).Copy()
+
+
+
+        'PresAModifer = PwP.Presentations.Open("C:\Users\zacha\Desktop\TEST.pptx", , , False)
+        'PresAModifer.Slides().Paste()
+        'PresAModifer.Save()
+        'PresAModifer.Close()
+        'PresAModifer = PwP.Presentations.Open("C:\Users\zacha\Desktop\TEST2.pptx", , , False)
+        'PresAModifer.Slides().Paste()
+        'PresAModifer.Save()
+        'PresAModifer.Close()
+
+
+
+        'Close the presentation without saving changes and quit PowerPoint.
+        PresDeBase.Saved = True
+        PresDeBase.Close()
+        PresDeBase = Nothing
+
+
+
+        Call ListFilesInFolder(DossierExport, IncludeSubFolder.Checked)
+        'Call test(FicherPPTdeBASE)
+        'Call test("C:\Users\zacha\Desktop\CREDITS DEF.pptx")
+        PwP.Quit()
+        PwP = Nothing
+        GC.Collect()
+        Me.Close()
+    End Sub
+
+    Sub OuvreColleLaSlideSauveEtFerme(nom As String)
+
+        PresAModifer = PwP.Presentations.Open(nom, , , False)
+        If StartSlide.Checked Then
+            PresAModifer.Slides().Paste(1)
+        Else
+            PresAModifer.Slides().Paste()
+        End If
+
+        PresAModifer.Save()
+        PresAModifer.Close()
+
+
+
+    End Sub
+
+    Sub ListFilesInFolder(ByVal xFolderName As String, ByVal xIsSubfolders As Boolean)
+        Dim xFileSystemObject As Object
+        Dim xFolder As Object
+        Dim xSubFolder As Object
+        Dim xFile As Object
+        xFileSystemObject = CreateObject("Scripting.FileSystemObject")
+        xFolder = xFileSystemObject.GetFolder(xFolderName)
+
+        For Each xFile In xFolder.Files
+            Dim ext = Microsoft.VisualBasic.Right(xFile.name, 4)
+            If ext = ".ppt" Or ext = "pptx" Or ext = "pptm" Or ext = "ppsm" Then
+                Debug.Print(xFile.name)
+                Call OuvreColleLaSlideSauveEtFerme(xFile.path)
+            End If
+            'Call ajoutDeLaSlide(xFile)
+        Next xFile
+        If xIsSubfolders Then
+            For Each xSubFolder In xFolder.SubFolders
+                Call ListFilesInFolder(xSubFolder.Path, True)
+            Next xSubFolder
+        End If
+        xFile = Nothing
+        xFolder = Nothing
+        xFileSystemObject = Nothing
+    End Sub
+
+
+
+End Class
+
+
+
+'Sub ARCHIVE_OuvrirLeFichier(leFichier As String)
+'    Const sPic = "c:\Program Files (x86)\Microsoft Office\root\CLIPART\PUB60COR\PH02746U.BMP"
+
+'    Dim PwP As PowerPoint.Application
+'    Dim oPres As PowerPoint.Presentation
+'    Dim oSlide As PowerPoint.Slide
+
+'    'Start Powerpoint and make its window visible but minimized.
+'    PwP = New PowerPoint.Application()
+'    PwP.Visible = True
+'    PwP.WindowState = PowerPoint.PpWindowState.ppWindowMinimized
+
+'    'oPres.Open(FileName:="D:\Dropbox\Boulot\MaMa Xls to PPT\2021\CREDIT TEST.pptx")
+'    'PwP.Presentations.Open(FicherPPTdeBASE)
+'    Dim sTemplate As String
+'    sTemplate = leFichier
+'    'Const sTemplate = leFichier
+'    'Create a new presentation based on the specified template.
+'    oPres = PwP.Presentations.Open(sTemplate, , , True)
+
+'    'Build Slide #1:
+'    'Add text to the slide, change the font and insert/position a 
+'    'picture on the first slide.
+'    oSlide = oPres.Slides.Add(1, PowerPoint.PpSlideLayout.ppLayoutTitleOnly)
+'    With oSlide.Shapes.Item(1).TextFrame.TextRange
+'        .Text = "My Sample Presentation"
+'        .Font.Name = "Comic Sans MS"
+'        .Font.Size = 48
+'    End With
+'    oSlide.Shapes.AddPicture(sPic, False, True, 150, 150, 500, 350)
+'    oSlide = Nothing
+
+'    'Build Slide #2:
+'    'Add text to the slide title, format the text. Also add a chart to the
+'    'slide and change the chart type to a 3D pie chart.
+'    oSlide = oPres.Slides.Add(2, PowerPoint.PpSlideLayout.ppLayoutTitleOnly)
+'    With oSlide.Shapes.Item(1).TextFrame.TextRange
+'        .Text = "My Chart"
+'        .Font.Name = "Comic Sans MS"
+'        .Font.Size = 48
+'    End With
+'    Dim oChart As Graph.Chart
+'    oChart = oSlide.Shapes.AddOLEObject(150, 150, 480, 320,
+'                "MSGraph.Chart.8").OLEFormat.Object
+'    oChart.ChartType = Graph.XlChartType.xl3DPie
+'    oChart = Nothing
+'    oSlide = Nothing
+
+'    'Build Slide #3:
+'    'Add a text effect to the slide and apply shadows to the text effect.
+'    oSlide = oPres.Slides.Add(3, PowerPoint.PpSlideLayout.ppLayoutBlank)
+'    oSlide.FollowMasterBackground = False
+'    Dim oShape As PowerPoint.Shape
+'    oShape = oSlide.Shapes.AddTextEffect(Office.MsoPresetTextEffect.msoTextEffect27,
+'        "The End", "Impact", 96, False, False, 230, 200)
+'    oShape.Shadow.ForeColor.SchemeColor = PowerPoint.PpColorSchemeIndex.ppForeground
+'    oShape.Shadow.Visible = True
+'    oShape.Shadow.OffsetX = 3
+'    oShape.Shadow.OffsetY = 3
+'    oShape = Nothing
+'    oSlide = Nothing
+
+'    'Modify the slide show transition settings for all 3 slides in
+'    'the presentation.
+'    Dim SlideIdx(3) As Integer
+'    SlideIdx(0) = 1
+'    SlideIdx(1) = 2
+'    SlideIdx(2) = 3
+'    With oPres.Slides.Range(SlideIdx).SlideShowTransition
+'        .AdvanceOnTime = True
+'        .AdvanceTime = 3
+'        .EntryEffect = PowerPoint.PpEntryEffect.ppEffectBoxOut
+'    End With
+'    Dim oSettings As PowerPoint.SlideShowSettings
+'    oSettings = oPres.SlideShowSettings
+'    oSettings.StartingSlide = 1
+'    oSettings.EndingSlide = 3
+
+
+
+'    'Close the presentation without saving changes and quit PowerPoint.
+'    oPres.Saved = False
+'    oPres.Save()
+'    oPres.Close()
+'    oPres = Nothing
+'    PwP.Quit()
+'    PwP = Nothing
+'    GC.Collect()
+'End Sub
+
+'Option Explicit On
+
+
+'Sub MainDecoupage()
+'    Formulaire.Show
+'    If OnYVa = False Then Exit Sub
+'    MsgBox "Attention ! Ce Proccessus peux etre long ! Merci de ne rien faire avec votre ordinateur avant le message de fin de traitement !", vbExclamation
+'DossierExport = DossierExport & "\"
+
+''Ouverture du fichier
+'Set PPTdeBASE = Application.Presentations.Open(FileName:=FicherPPTdeBASE)
+
+''boucle sur tout les fichiers du dossier
+'Call ListFilesInFolder(DossierExport, True)
+
+'    'on referme la slide de base
+'    PPTdeBASE.Close
+
+'    MsgBox "Voila, vos fichiers sont maintenant crées ! A bientôt !", vbExclamation
+''Application.Quit
+'End Sub
+'Sub ajoutDeLaSlide(ByVal fichierPPTenCOUR As String)
+'    Dim PPTenCOUR As PowerPoint.Presentation
+'    Set PPTenCOUR = Application.Presentations.Open(FileName:=fichierPPTenCOUR)
+'    PPTdeBASE.Slides(1).Copy
+'    PPTenCOUR.Slides.Paste
+'    PPTenCOUR.Save
+'    PPTenCOUR.Close
+'End Sub
+
+'Sub test()
+'    Call ListFilesInFolder("D:\OneDrive\Famille et amis\Balthazar", True)
+'End Sub
+'Sub ListFilesInFolder(ByVal xFolderName As String, ByVal xIsSubfolders As Boolean)
+'    Dim xFileSystemObject As Object
+'    Dim xFolder As Object
+'    Dim xSubFolder As Object
+'    Dim xFile As Object
+'Set xFileSystemObject = CreateObject("Scripting.FileSystemObject")
+'Set xFolder = xFileSystemObject.GetFolder(xFolderName)
+
+'For Each xFile In xFolder.Files
+'        Call ajoutDeLaSlide(xFile)
+'    Next xFile
+'    If xIsSubfolders Then
+'        For Each xSubFolder In xFolder.SubFolders
+'            Call ListFilesInFolder(xSubFolder.Path, True)
+'        Next xSubFolder
+'    End If
+'Set xFile = Nothing
+'Set xFolder = Nothing
+'Set xFileSystemObject = Nothing
+'End Sub
+
